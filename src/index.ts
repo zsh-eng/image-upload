2ae96d803276f9f5c6d53dbe96f4d3b746f35778
sheng <49238630+zsh-eng@users.noreply.github.com>
2024-04-21 17:11:54 +0800

    feat: set up image upload worker

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,45 @@
-import { Hono } from 'hono'
-import { poweredBy } from 'hono/powered-by'
+import { Hono } from 'hono';
+import { poweredBy } from 'hono/powered-by';
+import { bearerAuth } from 'hono/bearer-auth';
 
-const app = new Hono()
+type Bindings = {
+  TOKEN: string;
+  BASE_URL: string;
+  IMAGE_BUCKET: R2Bucket;
+};
 
-app.use('*', poweredBy())
+const app = new Hono<{ Bindings: Bindings }>();
+
+app.use('*', poweredBy());
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+  return c.text('Hello Hono!');
+});
 
-export default app
+app.put(
+  '/upload',
+  async (c, next) => bearerAuth({ token: c.env.TOKEN })(c, next),
+  async (c) => {
+    const body = await c.req.parseBody();
+    const file = body['file'] as File;
+    if (!file) {
+      return c.json({ error: 'Invalid file' }, 400);
+    }
+
+    const fileType = file.type;
+    if (!fileType.startsWith('image/')) {
+      return c.json({ error: 'Invalid file type' }, 400);
+    }
+
+    const uuid = crypto.randomUUID();
+    const bucket = c.env.IMAGE_BUCKET;
+    const key = `images/${uuid}`;
+    await bucket.put(key, file);
+
+    const url = `${c.env.BASE_URL}/${key}`;
+
+    return c.json({ imageLink: url });
+  }
+);
+
+export default app;
